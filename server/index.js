02f8d9a4f1f9811da33885b45a27a92b1590b15c
davisbento <contato@davisbento.dev>
2023-10-08 15:13:00 -0300

    app configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
+import byteSize from 'byte-size';
+import csvtojson from 'csvtojson';
+import { createReadStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
 import { createServer } from 'node:http';
+import { Readable, Transform, Writable } from 'node:stream';
+import { TransformStream } from 'node:stream/web';
 
-const server = createServer((req, res) => {
-	res.end('Hello World');
+const PORT = 3000;
+// curl -N http://localhost:3000
+const server = createServer(async (req, res) => {
+	const filename = './data/animeflv.csv';
+	const { size } = await stat(filename);
+
+	console.log('processing', byteSize(size).toString());
+
+	await Readable.toWeb(createReadStream(filename))
+		.pipeThrough(Transform.toWeb(csvtojson()))
+		.pipeThrough(
+			new TransformStream({
+				async transform(chunk, controller) {
+					// chunk is a buffer
+					const data = JSON.parse(Buffer.from(chunk));
+					const mappedData = JSON.stringify({
+						title: data.titulo,
+						description: data.description,
+						url: data.url_anime
+					});
+
+					// just to make sure that our data has as separator
+					controller.enqueue(mappedData.concat('\n'));
+				}
+			})
+		)
+		.pipeTo(Writable.toWeb(res));
 });
 
-server.listen(3000);
+server.listen(PORT).on('listening', () => console.log(`Server is listening on port ${PORT}`));
