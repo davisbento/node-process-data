bb43067f073626a4c9e3dd83b4ab7008b55a5d4a
davisbento <contato@davisbento.dev>
2023-10-08 18:46:56 -0300

    general improvements

--- app/index.js
+++ app/index.js
@@ -1,15 +1,89 @@
 const API_URL = 'http://localhost:3000';
 
-async function consumeAPI() {
-	const response = await fetch(API_URL);
+async function consumeAPI(signal) {
+	const response = await fetch(API_URL, {
+		signal
+	});
 
-	const reader = response.body.pipeTo(
-		new WritableStream({
-			async write(chunk) {
-				console.log(chunk);
-			}
-		})
-	);
+	const reader = response.body.pipeThrough(new TextDecoderStream()).pipeThrough(parseNDJSON());
+
+	return reader;
 }
 
-await consumeAPI();
+// this function will make sure that if two chunks come from a single transmission
+// converit and split in break lines
+// given {}\n{}\n{}\n
+// it will return {} {} {}
+
+function parseNDJSON() {
+	return new TransformStream({
+		async transform(chunk, controller) {
+			for (const item of chunk.split('\n')) {
+				if (!item.length) continue;
+
+				try {
+					controller.enqueue(JSON.parse(item));
+				} catch (error) {
+					// this expection is a common problem that we wont handle for now
+					// if the arrived data is not completed, it should be stored in memory until the next transmission
+				}
+			}
+		}
+	});
+}
+
+let counter = 0;
+let elementCounter = 0;
+
+function appendToHtml(element) {
+	return new WritableStream({
+		write({ title, description, url }) {
+			const card = `
+      <article>
+        <div class="text">
+          <h3>[${++counter}] ${title}</h3>
+          <p>${description.slice(0, 100)}</p>
+          <a href="${url}">Go to</a>
+        </div>
+      </article>
+      `;
+
+			// if (++elementCounter > 20) {
+			// 	element.innerHTML = card;
+			// 	elementCounter = 0;
+			// 	return;
+			// }
+
+			element.innerHTML += card;
+		},
+		abort(reason) {
+			console.log('aborted*', reason);
+		}
+	});
+}
+
+const [start, stop, clear, cards] = ['start', 'stop', 'clear', 'cards'].map((id) => document.getElementById(id));
+
+let abortController = new AbortController();
+
+start.addEventListener('click', async () => {
+	try {
+		const reader = await consumeAPI(abortController.signal);
+		await reader.pipeTo(appendToHtml(cards), { signal: abortController.signal });
+	} catch (error) {
+		if (!error.message.includes('abort')) throw error;
+	}
+});
+
+stop.addEventListener('click', () => {
+	abortController.abort();
+	console.log('aborting...');
+
+	abortController = new AbortController();
+});
+
+clear.addEventListener('click', () => {
+	cards.innerHTML = '';
+	counter = 0;
+	elementCounter = 0;
+});
--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ const server = createServer(async (req, res) => {
 		return;
 	}
 
+	let counter = 0;
 	const filename = './data/animeflv.csv';
 	const { size } = await stat(filename);
 
@@ -29,25 +30,35 @@ const server = createServer(async (req, res) => {
 	try {
 		res.writeHead(200, headers);
 
+		const abortController = new AbortController();
+
+		req.once('close', (_) => {
+			console.log('connection was closed!', counter);
+			abortController.abort();
+		});
+
 		await Readable.toWeb(createReadStream(filename))
-			.pipeThrough(Transform.toWeb(csvtojson()))
+			.pipeThrough(Transform.toWeb(csvtojson({ headers: ['title', 'description', 'url_anime'] })))
 			.pipeThrough(
 				new TransformStream({
 					async transform(chunk, controller) {
 						// chunk is a buffer
 						const data = JSON.parse(Buffer.from(chunk));
 						const mappedData = JSON.stringify({
-							title: data.titulo,
+							title: data.title,
 							description: data.description,
 							url: data.url_anime
 						});
+
+						counter++;
+						await setTimeout(() => {}, 500);
 
 						// just to make sure that our data has as separator
 						controller.enqueue(mappedData.concat('\n'));
 					}
 				})
 			)
-			.pipeTo(Writable.toWeb(res));
+			.pipeTo(Writable.toWeb(res), { signal: abortController.signal });
 	} catch (error) {
 		console.error(error);
 	}
